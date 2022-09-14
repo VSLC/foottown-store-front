f1d77a01591c9b0b803cb4fafecfc74a739f4e4c
Voller <voller.silas@gmail.com>
2022-09-14 15:59:06 -0300

    build: Modifying some dependencies , app and index working

---
+++ src/app.js
@@ -0,0 +1,11 @@
+
+import RegistrationPage from "./pages/RegistrationPage"
+const App = () => {
+    return (
+        <>
+            <RegistrationPage />
+        </>
+    )
+}
+
+export default App;
